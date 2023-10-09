9438f83395b081c5a18578eece98e14317a3b8a7
Lucas Matias <lucasdamatias@gmail.com>
2023-10-09 20:07:45 -0300

    First Deploy

---
+++ CalcMedia/Form1.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace CalcMedia {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.iDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.mediaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.horaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.mediasBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.bancoDataSet4 = new CalcMedia.BancoDataSet4();
+            this.mediasTableAdapter = new CalcMedia.BancoDataSet4TableAdapters.MediasTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mediasBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bancoDataSet4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.3F);
+            this.label1.Location = new System.Drawing.Point(400, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "AVERAGE CALCULATOR";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 157);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Primeira Nota:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(93, 155);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(76, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(326, 154);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(74, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(236, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Segunda Nota:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 218);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Terceira Nota:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(326, 212);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(74, 20);
+            this.textBox4.TabIndex = 8;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged_1);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(247, 215);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Quarta Nota:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(120, 284);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(231, 284);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Limpar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(170, 326);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(84, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Média do Aluno:";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(93, 215);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(74, 20);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(200, 365);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.27F);
+            this.label8.Location = new System.Drawing.Point(161, 97);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(121, 15);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Calcule suas Médias";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.27F);
+            this.label9.Location = new System.Drawing.Point(679, 97);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(117, 15);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "Visualize o Histórico";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.iDDataGridViewTextBoxColumn,
+            this.mediaDataGridViewTextBoxColumn,
+            this.dataDataGridViewTextBoxColumn,
+            this.horaDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.mediasBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(561, 142);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(345, 197);
+            this.dataGridView1.TabIndex = 16;
+            // 
+            // iDDataGridViewTextBoxColumn
+            // 
+            this.iDDataGridViewTextBoxColumn.DataPropertyName = "ID";
+            this.iDDataGridViewTextBoxColumn.HeaderText = "ID";
+            this.iDDataGridViewTextBoxColumn.Name = "iDDataGridViewTextBoxColumn";
+            this.iDDataGridViewTextBoxColumn.Visible = false;
+            // 
+            // mediaDataGridViewTextBoxColumn
+            // 
+            this.mediaDataGridViewTextBoxColumn.DataPropertyName = "Media";
+            this.mediaDataGridViewTextBoxColumn.HeaderText = "Media";
+            this.mediaDataGridViewTextBoxColumn.Name = "mediaDataGridViewTextBoxColumn";
+            // 
+            // dataDataGridViewTextBoxColumn
+            // 
+            this.dataDataGridViewTextBoxColumn.DataPropertyName = "Data";
+            this.dataDataGridViewTextBoxColumn.HeaderText = "Data";
+            this.dataDataGridViewTextBoxColumn.Name = "dataDataGridViewTextBoxColumn";
+            // 
+            // horaDataGridViewTextBoxColumn
+            // 
+            this.horaDataGridViewTextBoxColumn.DataPropertyName = "Hora";
+            this.horaDataGridViewTextBoxColumn.HeaderText = "Hora";
+            this.horaDataGridViewTextBoxColumn.Name = "horaDataGridViewTextBoxColumn";
+            // 
+            // mediasBindingSource
+            // 
+            this.mediasBindingSource.DataMember = "Medias";
+            this.mediasBindingSource.DataSource = this.bancoDataSet4;
+            // 
+            // bancoDataSet4
+            // 
+            this.bancoDataSet4.DataSetName = "BancoDataSet4";
+            this.bancoDataSet4.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // mediasTableAdapter
+            // 
+            this.mediasTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(947, 435);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Average Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mediasBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bancoDataSet4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private BancoDataSet4 bancoDataSet4;
+        private System.Windows.Forms.BindingSource mediasBindingSource;
+        private BancoDataSet4TableAdapters.MediasTableAdapter mediasTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn iDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn mediaDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn horaDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ CalcMedia/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CalcMedia {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+        
+        private void label1_Click(object sender, EventArgs e) {
+        }
+        private void Form1_Load(object sender, EventArgs e) {
+            // TODO: This line of code loads data into the 'bancoDataSet4.Medias' table. You can move, or remove it, as needed.
+            this.mediasTableAdapter.Fill(this.bancoDataSet4.Medias);
+
+        }
+        private void label2_Click(object sender, EventArgs e) {
+        }
+        private void textBox1_TextChanged_1(object sender, EventArgs e) {
+            textBox1.MaxLength = 3;
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e) {
+            textBox2.MaxLength = 3;
+        }
+        private void textBox3_TextChanged(object sender, EventArgs e) {
+            textBox3.MaxLength = 3;
+        }
+        private void textBox4_TextChanged_1(object sender, EventArgs e) {
+            textBox4.MaxLength = 3;
+        }
+        private void label3_Click(object sender, EventArgs e) {
+        }
+        private void label4_Click(object sender, EventArgs e) {
+        }
+
+        private void label7_Click(object sender, EventArgs e) {
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            float som, med, val;
+            som = 0;
+
+            try {
+
+                foreach (Control controle in this.Controls) {
+                    if (controle is TextBox) {
+                        val = Convert.ToSingle(((TextBox)controle).Text);
+                        som += val;
+                    }
+
+                    med = som / 4;
+                    this.Controls["label7"].Text = med.ToString();
+
+                    if (med > 6) {
+                        this.Controls["label7"].ForeColor = Color.Green;
+                    } else {
+                        this.Controls["label7"].ForeColor = Color.Red;
+                    }
+
+                    var date = DateTime.Today.ToString("dd/MM/yyyy");
+                    var time = DateTime.Now.ToString("hh:mm:ss");
+
+                    StreamWriter x;
+
+                    string Caminho = "Historico de Notas.txt";
+
+                    x = File.AppendText(Caminho);
+
+                    x.WriteLine("SEU HISTÓRICO");
+                    x.WriteLine("___________________________________________");
+                    x.WriteLine("Nota: " + med.ToString());
+                    x.WriteLine("Data e Hora: " + date.ToString() + " | " + time.ToString());
+                    x.WriteLine();
+                    x.WriteLine();
+
+                    x.Close();
+                }
+
+            } catch (Exception) {
+                MessageBox.Show("Dados incorretos, por favor verifique as notas digitadas e tente novamente", "Atenção!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                this.Controls["label7"].Text = "";
+            }
+
+        }
+        private void button2_Click(object sender, EventArgs e) {
+
+            foreach (Control controle in this.Controls) {
+
+                if (controle is TextBox) {
+                    ((TextBox)controle).Text = "";
+                }
+
+                this.Controls["label7"].Text = "";
+            }
+        }
+        private void label6_Click(object sender, EventArgs e) {
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e) { 
+        }
+    }
+}
